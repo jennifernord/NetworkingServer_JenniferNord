7453aef6daf079fa000fd94826971755db710961
jennifer.nord <jennifer.nord@elev.ga.ntig.se>
2024-05-08 10:38:25 +0200

    påbörjat avskrivning av kod

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.*;
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ServerSocket server = null;
+        Socket client;
+
+        int portnumber = 1234;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        try{
+            server = new ServerSocket(portnumber);
+        }catch (IOException e){
+            System.out.println("Cannot open socket." +ie);
+            System.exit(1);
+        }
+        System.out.println("Serversocket is created." +server);
+
+        while (true){
+            try{
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+            }
+        }
     }
 }
